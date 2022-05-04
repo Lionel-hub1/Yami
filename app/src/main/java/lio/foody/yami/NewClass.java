af3c75fb65dc04b2c7e54204bee66535411d3fa2
lionel-hub1 <ndabagajeanlionel@gmail.com>
2022-05-04 16:10:53 +0000

    ok

---
+++ app/src/main/java/lio/foody/yami/NewClass.java
@@ -0,0 +1,11 @@
+package lio.foody.yami;
+
+public class NewClass {
+    String myString;
+    int myNumber;
+
+    public NewClass(String myString, int myNumber) {
+        this.myString = myString;
+        this.myNumber = myNumber;
+    }
+}
