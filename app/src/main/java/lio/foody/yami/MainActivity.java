d4f7533a2be9a88e036fab6e30e8a8f41915a1a2
lionel-hub1 <ndabagajeanlionel@gmail.com>
2022-07-25 21:03:58 +0000

    MainActivity working well

--- app/src/main/java/lio/foody/yami/MainActivity.java
+++ app/src/main/java/lio/foody/yami/MainActivity.java
@@ -1,108 +1,153 @@
 package lio.foody.yami;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
-import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.TextUtils;
-import android.util.Patterns;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.auth.AuthResult;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
-    Button btnLogin, btnRegister, login_as_admin_btn;
-    Coms coms;
-    TextView usernameInput;
-    EditText emailEt, passwordEt;
-    ProgressDialog progressDialog;
+    //This information was from HomePage.java
+
     FirebaseAuth mAuth;
-    FirebaseUser firebaseUser;
+    FirebaseUser fbUser;
+    Context context;
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        emailEt = findViewById(R.id.usernameInput);
-        passwordEt = findViewById(R.id.passwordInput);
-        coms=new Coms(this);
-        mAuth=FirebaseAuth.getInstance();
-        btnLogin    = findViewById(R.id.btnLogin);
-        btnRegister = findViewById(R.id.btnRegister);
-        login_as_admin_btn = findViewById(R.id.login_as_admin_btn);
-        usernameInput=findViewById(R.id.usernameInput);
-        progressDialog=new ProgressDialog(this);
-        progressDialog.setTitle("Logging in ...");
-        progressDialog.setCanceledOnTouchOutside(false);
-//        ActionBar actionBar = getSupportActionBar();
-//        actionBar.setTitle("Login");
+        setContentView(R.layout.activity_home_page);
+        mAuth = FirebaseAuth.getInstance();
+        fbUser = mAuth.getCurrentUser();
 
-//        btnLogin.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Intent mIntent2 = new Intent(getApplicationContext(),HomePage.class);
-//                mIntent2.putExtra("keyname",usernameInput.getText().toString());
-//                startActivity(mIntent2);
-//            }
-//        });
-        btnLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //Check validity of email and password
-                String email=emailEt.getText().toString();
-                String pass=passwordEt.getText().toString();
-                if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
-                    emailEt.setError("Invalid email");
-                    return;
-                }
-                if (TextUtils.isEmpty(pass)){
-                    passwordEt.setError("Password is required");
-                    return;
-                }
-                progressDialog.show();
-                mAuth.signInWithEmailAndPassword(email,pass)
-                        .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
-                            @Override
-                            public void onSuccess(AuthResult authResult) {
-                                progressDialog.dismiss();
-                                startActivity(new Intent(getApplicationContext(),HomePage.class));
-                                finish();
-                            }
-                        })
-                        .addOnFailureListener(new OnFailureListener() {
-                            @Override
-                            public void onFailure(@NonNull Exception e) {
-                                progressDialog.dismiss();
-                                coms.message("Error Login",e.getMessage());
-                            }
-                        });
-            }
-        });
-        btnRegister.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(getApplicationContext(), Register.class));
-                finish();
-            }
-        });
-        login_as_admin_btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(getApplicationContext(), Register.class));
-                finish();
-            }
-        });
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+
+        bottomNavigationView.setOnItemSelectedListener(navListener);
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainerView, new Home()).commit();
+
+//        Intent myIntent =getIntent();
+//        Toast.makeText(getApplicationContext(), "Welcome "+myIntent.getStringExtra("keyname"), Toast.LENGTH_LONG).show();
 
     }
+
+
+    NavigationBarView.OnItemSelectedListener navListener = new NavigationBarView.OnItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment fragmentContainerView = null;
+
+            switch (item.getItemId()){
+                case R.id.home4:
+                    if (fbUser!=null){
+                        //go to the other page
+                        fragmentContainerView =  new Home();
+                    }
+                    else{
+                        //require login
+                        fragmentContainerView =  new Home();
+                    }
+                    break;
+
+                case R.id.findFood:
+                    if (fbUser!=null){
+                        //go to the other page
+                        fragmentContainerView =  new FindFood();
+                    }
+                    else{
+                        //require login
+                        fragmentContainerView =  new LoginFrag();
+                    }
+                    break;
+
+                case R.id.learnHowTo:
+                    if (fbUser!=null){
+                        //go to the other page
+                        fragmentContainerView =  new LearnHowTo();
+                    }
+                    else{
+                        //require login
+                        fragmentContainerView =  new LoginFrag();
+                    }
+                    break;
+
+                case R.id.settings:
+                    if (fbUser!=null){
+                        //go to the other page
+                        fragmentContainerView =  new Profile();
+                    }
+                    else{
+                        //require login
+                        fragmentContainerView =  new LoginFrag();
+                    }
+                    break;
+            }
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainerView, fragmentContainerView).commit();
+
+
+            return true;
+        }
+    };
+
+
+
+
+    //------------------------------------------------------------------------------------------------------------
+    /*@Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.top_menu, menu);
+        return true;
+    }*/
+    //+++++++++++++++++++++++++++++++=
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.top_menu,menu);
+        MenuItem mnLogin = menu.findItem(R.id.mnuLogin);
+        MenuItem mnLogout = menu.findItem(R.id.mnuLogout);
+        MenuItem mnConnectFoodMenu = menu.findItem(R.id.connectFoodMenu);
+        //write logic: if some one logs in, login menu disapears and we show logout
+        if (fbUser!=null){
+            //hide login show logout
+            mnConnectFoodMenu.setVisible(true);
+            mnLogin.setVisible(false);
+            mnLogout.setVisible(true);
+        }
+        else{
+            //hide logout show login
+            mnConnectFoodMenu.setVisible(false);
+            mnLogin.setVisible(true);
+            mnLogout.setVisible(false);
+        }
+        return true;
+    }
+    //----------------------------------------------------------------------------------------------------------
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.connectFoodMenu:
+                startActivity(new Intent(getApplicationContext(), ConnectFoodActivity.class));
+                break;
+            case R.id.mnuLogin:
+                startActivity(new Intent(getApplicationContext(), LoginPageActivity.class));
+                break;
+            case R.id.mnuLogout:
+                mAuth.signOut();
+                startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
 }
